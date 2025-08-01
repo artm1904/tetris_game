4a5580af3764a90bc35558e57eb823ff159da970
artm1904 <artmsavchenko@yandex.ru>
2025-08-01 19:21:10 +0300

    Create the Color class

---
+++ src/Colors/colors.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "raylib.h"
+#include <vector>
+
+extern cosnt Color darkGrey;
+extern cosnt Color green;
+extern cosnt Color red;
+extern cosnt Color orange;
+extern cosnt Color yellow;
+extern cosnt Color purple;
+extern cosnt Color cyan;
+extern cosnt Color blue;
+
+
+std::vector<Color> GetCellColors();
+
